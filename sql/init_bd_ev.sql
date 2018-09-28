f320b4eaf4290cd5941dad449be07d4dd964746d
NicolasDEVOUCOUX <43570092+NicolasDEVOUCOUX@users.noreply.github.com>
2018-09-28 12:08:03 +0200

    Update init_bd_ev.sql

--- sql/init_bd_ev.sql
+++ sql/init_bd_ev.sql
@@ -463,8 +463,8 @@ COMMENT ON TABLE m_espace_vert.geo_ev_espece_invasive
 IS 'table géographique ponctuelle répertoriant les espèces invasives';
 COMMENT ON COLUMN m_espace_vert.geo_ev_espece_invasive.id IS 'Identifiant unique de l''objet espace vert';
 COMMENT ON COLUMN m_espace_vert.geo_ev_espece_invasive.nom_esp IS 'Nom de l''espèce végétale exotique envahissante'
-COMMENT ON COLUMN m_espace_vert.an_ev_type.insee IS 'Code Insee de la commune';
-COMMENT ON COLUMN m_espace_vert.an_ev_type.commune IS 'Nome de la commune';
+COMMENT ON COLUMN m_espace_vert.geo_ev_espece_invasive.insee IS 'Code Insee de la commune';
+COMMENT ON COLUMN m_espace_vert.geo_ev_espece_invasive.commune IS 'Nome de la commune';
 COMMENT ON COLUMN m_espace_vert.geo_ev_espece_invasive.geom IS 'Géométrie de l''objet';
 
 
