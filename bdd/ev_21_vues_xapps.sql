a48aa92ab2bad5b60da469df1b0c5045c4645be3
gregbodet <37414914+gregbodet@users.noreply.github.com>
2020-11-07 09:49:01 +0100

    Update and rename tri_21_vues_xapps.sql to ev_21_vues_xapps.sql

---
+++ bdd/ev_21_vues_xapps.sql
@@ -0,0 +1,9 @@
+/* ESPACE VERT V1.0*/
+/* Creation des vues applicatives */
+/* ev_21_vues_xapps.sql */
+/*PostGIS*/
+
+/* Propriétaire : GeoCompiegnois - http://geo.compiegnois.fr/ */
+/* Auteur : Grégory Bodet */
+
+/* SANS OBJET */
