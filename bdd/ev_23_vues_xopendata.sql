c8bcd5655a1ea8a553c5de5822d670164e0e02dd
gregbodet <37414914+gregbodet@users.noreply.github.com>
2020-11-07 09:46:25 +0100

    Update and rename tri_23_vues_xopendata.sql to ev_23_vues_xopendata.sql

---
+++ bdd/ev_23_vues_xopendata.sql
@@ -0,0 +1,13 @@
+/* ESPACE VERT V1.0*/
+/* Creation des vues applicatives publiques */
+/* ev_23_vues_xopendata.sql */
+/*PostGIS*/
+
+/* Propriétaire : GeoCompiegnois - http://geo.compiegnois.fr/ */
+/* Auteur : Grégory Bodet */
+
+
+
+
+                                                    
+
