ec4c71d7bf5178454ca9a84ae5b5b7842f316c88
gregbodet <37414914+gregbodet@users.noreply.github.com>
2020-11-07 09:50:11 +0100

    Update and rename tri_20_vues_gestion.sql to ev_20_vues_gestion.sql

---
+++ bdd/ev_20_vues_gestion.sql
@@ -0,0 +1,9 @@
+/* ESPACE VERT V1.0*/
+/* Creation des vues de gestion */
+/* ev_20_vues_gestion.sql */
+/*PostGIS*/
+
+/* Propriétaire : GeoCompiegnois - http://geo.compiegnois.fr/ */
+/* Auteur : Grégory Bodet */
+
+Sans objet
