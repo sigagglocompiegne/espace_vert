1ab0008148a8d360ed9dc83692df852379380203
gregbodet <37414914+gregbodet@users.noreply.github.com>
2021-03-19 07:56:53 +0100

    Create ev_00_trace.sql

---
+++ bdd/ev_00_trace.sql
@@ -0,0 +1,20 @@
+/*ESPACE VERT V1.0*/
+/*Creation du fichier trace qui permet de suivre l'évolution du code*/
+/* ev_00_trace.sql */
+/*PostGIS*/
+
+/* Propriétaire : GeoCompiegnois - http://geo.compiegnois.fr/ */
+/* Auteur : Bodet Grégory */
+
+/*  
+ 
+  Liste des dépendances :
+  schéma          | table                 | description                                                   | usage
+*/
+
+/*
+#################################################################### SUIVI CODE SQL ####################################################################
+
+2021-03-19 : GB / initialisation du code du nouveau modèle de données pour un inventaire cartographique (dernière version fonctionnel)
+
+
