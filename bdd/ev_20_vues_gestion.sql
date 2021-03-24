b8d9188a65cc6abc2f9a551f0c7ac91e3aadd5d0
gregbodet <37414914+gregbodet@users.noreply.github.com>
2021-03-24 14:16:50 +0100

    Update ev_20_vues_gestion.sql

--- bdd/ev_20_vues_gestion.sql
+++ bdd/ev_20_vues_gestion.sql
@@ -48,71 +48,6 @@ DROP VIEW IF EXISTS m_espace_vert_v2.geo_v_ev_arbre;
 -- les vues ci-dessous sont pour le moment uniquement pour la visualisation des données en attentant de passer à une gestion métier par
 -- le sezvice espace vert à l'issu de l'inventaire cartographique
 
--- View: m_espace_vert_v2.geo_v_ev_line
-
--- DROP VIEW m_espace_vert_v2.geo_v_ev_line;
-
-CREATE OR REPLACE VIEW m_espace_vert_v2.geo_v_ev_line
- AS
- SELECT o.idobjet,
-    o.idzone,
-    o.idsite,
-    o.idcontrat,
-    o.insee,
-    o.commune,
-    o.quartier,
-    o.doma_d,
-    o.doma_r,
-    o.typ,
-    o.sstyp,
-    o.srcgeom_sai,
-    o.srcdate_sai,
-    o.srcgeom_maj,
-    o.srcdate_maj,
-    o.op_sai,
-    o.op_maj,
-    o.dat_sai,
-    o.dat_maj,
-    o.observ,
-    l.long_m,
-    l.larg_cm,
-    l.geom::geometry(multilinestring,2154) AS geom
-   FROM m_espace_vert_v2.an_ev_objet o
-     JOIN m_espace_vert_v2.geo_ev_line l ON o.idobjet = l.idobjet;
-
-
--- View: m_espace_vert_v2.geo_v_ev_pct
-
--- DROP VIEW m_espace_vert_v2.geo_v_ev_pct;
-
-CREATE OR REPLACE VIEW m_espace_vert_v2.geo_v_ev_pct
- AS
- SELECT o.idobjet,
-    o.idzone,
-    o.idsite,
-    o.idcontrat,
-    o.insee,
-    o.commune,
-    o.quartier,
-    o.doma_d,
-    o.doma_r,
-    o.typ,
-    o.sstyp,
-    o.srcgeom_sai,
-    o.srcdate_sai,
-    o.srcgeom_maj,
-    o.srcdate_maj,
-    o.op_sai,
-    o.op_maj,
-    o.dat_sai,
-    o.dat_maj,
-    o.observ,
-    p.x_l93,
-    p.y_l93,
-    p.geom::geometry(point,2154) AS geom
-   FROM m_espace_vert_v2.an_ev_objet o
-     JOIN m_espace_vert_v2.geo_ev_pct p ON o.idobjet = p.idobjet
-   WHERE o.sstyp <> '01-01';
 
 -- View: m_espace_vert_v2.geo_v_ev_arbre
 
@@ -171,37 +106,7 @@ CREATE OR REPLACE VIEW m_espace_vert_v2.geo_v_ev_arbre
 	 JOIN m_espace_vert_v2.an_ev_arbre a ON o.idobjet = a.idobjet
    WHERE o.sstyp = '01-01';			      
 
--- View: m_espace_vert_v2.geo_v_ev_polygon
 
--- DROP VIEW m_espace_vert_v2.geo_v_ev_polygon;
-
-CREATE OR REPLACE VIEW m_espace_vert_v2.geo_v_ev_polygon
- AS
- SELECT o.idobjet,
-    o.idzone,
-    o.idsite,
-    o.idcontrat,
-    o.insee,
-    o.commune,
-    o.quartier,
-    o.doma_d,
-    o.doma_r,
-    o.typ,
-    o.sstyp,
-    o.srcgeom_sai,
-    o.srcdate_sai,
-    o.srcgeom_maj,
-    o.srcdate_maj,
-    o.op_sai,
-    o.op_maj,
-    o.dat_sai,
-    o.dat_maj,
-    o.observ,
-    p.sup_m2,
-    p.perimetre,
-    p.geom::geometry(multipolygon,2154) AS geom
-   FROM m_espace_vert_v2.an_ev_objet o
-     JOIN m_espace_vert_v2.geo_ev_polygon p ON o.idobjet = p.idobjet;
 
 
 
